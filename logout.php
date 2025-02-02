8784b4089f88827a92413a49a011599687b20253
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 14:34:47 +0100

    test i ri

--- logout.php
+++ logout.php
@@ -1,22 +1,9 @@
 <?php
 
-@include 'config.php';
-
 session_start();
 
-// Unset all session variables
-$_SESSION = [];
-
-// Destroy the session
 session_destroy();
 
-// Prevent caching
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0"); // HTTP 1.1.
-header("Pragma: no-cache"); // HTTP 1.0.
-header("Expires: 0"); // Proxies.
-
-// Redirect to the login page
-header("Location: login_form.php");
-exit(); // Ensure no further execution
+header('location:login_form.php')
 
 ?>

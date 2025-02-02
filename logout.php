6a7ea7197315f8c55dad6c61925f674b76088941
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 12:03:28 +0100

    logout improvements

--- logout.php
+++ logout.php
@@ -3,10 +3,17 @@
 @include 'config.php';
 
 session_start();
+
+// Unset and destroy the current session
+$_SESSION = array();
 session_unset();
 session_destroy();
 
-// Prevent caching
+// Start a new session with a fresh ID to prevent session fixation
+session_start();
+session_regenerate_id(true);
+
+// Prevent caching (so users can't access the previous session by pressing "Back")
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0"); // HTTP 1.1.
 header("Cache-Control: post-check=0, pre-check=0", false); // HTTP 1.0.
 header("Pragma: no-cache"); // HTTP 1.0.

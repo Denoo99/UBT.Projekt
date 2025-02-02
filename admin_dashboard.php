77b0c586e171e5e4632efb7088042d698a600961
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 01:52:58 +0100

    Testimi ri

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -11,7 +11,7 @@ if (isset($_POST['add_product'])) {
     $price = mysqli_real_escape_string($conn, $_POST['price']);
     
 
-    $target_dir = "uploads/";
+    $target_dir = "uploads";
     $target_file = $target_dir . basename($_FILES["image"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));

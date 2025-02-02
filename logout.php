a383b55304e610d01adb63fab5e77944347ab10e
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 14:22:19 +0100

    test2

--- logout.php
+++ logout.php
@@ -15,8 +15,8 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 
-// Redirect to the index page
-header('Location: index.html');
-session_destroy();
-exit(); // Ensure no further code is executed
+// Redirect to login page
+header("Location: index.html");
+exit(); // Ensure no further execution
+
 ?>

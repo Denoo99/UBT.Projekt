629f51d2dee7feacbf67e3e8c4747f7c939a7616
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 20:24:54 +0100

    last changes

--- admin_page.php
+++ admin_page.php
@@ -16,8 +16,7 @@ if(!isset($_SESSION['admin_name'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Faqja e Adminit</title>
-
+   <title>Absolute Mall</title>
    <link rel="stylesheet" href="style.css">
    <link rel="stylesheet" href="styles.css">
    

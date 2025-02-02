6364c8e4a3b750798a34b98a4fa8db6343586ba9
DenisUBT <dk70414@ubt-uni.net>
2025-02-02 13:55:00 +0100

    apet

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,11 +2,13 @@
 @include 'config.php';
 session_start();
 
+// Check if the user is logged in as admin
 if (!isset($_SESSION['admin_name'])) {
     header('location:login_form.php');
     exit();
 }
 
+// Handle adding a product
 if (isset($_POST['add_product'])) {
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $price = mysqli_real_escape_string($conn, $_POST['price']);
@@ -55,6 +57,7 @@ if (isset($_POST['add_product'])) {
     }
 }
 
+// Handle removing a product
 if (isset($_POST['remove_product'])) {
     $product_id = mysqli_real_escape_string($conn, $_POST['product_id']);
     
@@ -85,6 +88,7 @@ if (isset($_POST['remove_product'])) {
     }
 }
 
+// Fetch current products
 $products = mysqli_query($conn, "SELECT * FROM products");
 ?>
 
@@ -193,13 +197,13 @@ $products = mysqli_query($conn, "SELECT * FROM products");
             <li><a href="index.html">Ballina</a></li>
             <li><a href="products.php">Produktet</a></li>
             <li><a href="about.html">Rreth Nesh</a></li>
+            <li><a href="Forma e kontaktit.html">Kontakti</a></li>
             <?php if (isset($_SESSION['admin_name'])): ?>
                 <li><a href="admin_dashboard.php">Admin Dashboard</a></li>
                 <li><a href="logout.php">Log Out</a></li>
             <?php else: ?>
                 <li><a href="login_form.php">Login</a></li>
             <?php endif; ?>
-            <li><a href="Forma e kontaktit.html">Kontakti</a></li>
         </ul>
     </nav>
 </header>
